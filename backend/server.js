baed214a3c7be477eefb9e77fe120a967b4242cf
vickyaltitude <vignvick3005>
2025-01-17 14:38:33 +0530

    Fixed one bug

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use('/api/analytics',analyticsRoute)
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname,'../frontend/build')) )
-  app.use('*',(req,res)=>{
+  app.use('/',(req,res)=>{
       res.sendFile(path.resolve(__dirname,'../frontend','build','index.html'))
   })
 }
